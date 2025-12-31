2d6fdee721210719cda52f4f79f62ed055f73d76
vijayshagunkumar <141246960+vijayshagunkumar@users.noreply.github.com>
2025-12-31 13:11:59 +0530

    Create debounce.js

---
+++ js/utils/debounce.js
@@ -0,0 +1,25 @@
+// Simple debounce utility
+export function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+export function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
